9c95c57bff40f3f19589ecc5d93b7234f2e9aa27
Sadman-fahim <fahimsadman39@gmail.com>
2024-12-22 13:56:51 +0600

    first commit

---
+++ src/first/Datatypes.java
@@ -0,0 +1,16 @@
+package first;
+
+public class Datatypes {
+    public static void main (String[] args ){
+        int number=12;
+        float number2=12.34f;
+        double number3=23.3455;
+        char letter='a';
+        boolean bool=true;
+        System.out.println("Int numner :" +number);
+        System.out.println("float number2 : "+number2);
+        System.out.println("double number3 :"+number3);
+        System.out.println("letter :"+letter);
+        System.out.println("boolean :"+bool);
+    }
+}
